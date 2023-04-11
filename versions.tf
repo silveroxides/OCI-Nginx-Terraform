0de68f8c89b4622e22194ce564ae7a738b6e734d
Silver <65376327+silveroxides@users.noreply.github.com>
2023-04-12 00:51:16 +0100

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,16 @@
+
+      terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "4.115.0"
+    }
+  }
+}
+
+provider "oci" {
+  # Configuration options
+}
+
+
+    
